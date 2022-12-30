c86e153ea153bc83ffb6126200755001cf6d40d8
Rebeca Baruch <71520936+RebecaBaruch@users.noreply.github.com>
2022-12-30 11:02:39 -0300

    feat: chart component

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import './Expenses.css';
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -21,6 +22,7 @@ function Expenses(props) {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />  
             </Card>
         </div>
